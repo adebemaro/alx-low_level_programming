6c6671af247930f7d02d89571e84e701101bf8d9
adebemaro <adebemario@gmail.com>
2023-01-18 20:45:28 -0800

    first commit

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,22 +1,31 @@
-#ifndef _HEADER_
-#define _HEADER_
+#include "3-calc.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * struct op - Struct op
+ * get_op_func - ...
+ * @s: ...
  *
- * @op: The operator
- * @f: The function associated
+ * Return: ...
  */
-typedef struct op
+int (*get_op_func(char *s))(int, int)
 {
-	char *op;
-	int (*f)(int a, int b);
-} op_t;
+	op_t ops[] = {
+	{ "+", op_add },
+	{ "-", op_sub },
+	{ "*", op_mul },
+	{ "/", op_div }
+	{ "%", op_mod },
+	{ NULL, NULL }
+	};
+	int i = 0;
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
-#endif
+	while (i < 5)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);	
+		i++;
+	}
+
+	return (0);
+}
